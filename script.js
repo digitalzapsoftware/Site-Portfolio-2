86c622b2d6a5aadd0f615971300d801ccb519ff5
Evanilson <programadornilson@gmail.com>
2022-04-28 15:04:18 -0300

    adicionando estilos e o javascript.

---
+++ script.js
@@ -0,0 +1,17 @@
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.portfolio', {
+    duration: 2000
+})
+
+sr.reveal('.area-1', {
+     rotate: {x:0, y:80, z:0},
+     duration: 4000 });
+
+sr.reveal('.area-2', {
+    rotate: {x:0, y:80, z:0},
+    duration: 4000
+});
+
+sr.reveal('.area-3', {duration: 5000})
